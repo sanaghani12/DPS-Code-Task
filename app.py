a1ad63808ee33ca64cf341b2dec73f068235d092
sanaghani12 <91886253+sanaghani12@users.noreply.github.com>
2022-06-13 21:07:08 +0200

    Add file for Heroku App

--- app.py
+++ app.py
@@ -24,6 +24,22 @@ def predict():
         'index.html',
         prediction_text='prediction value is {}'.format(prediction))
 
+@app.route("/postapi/predict", methods=["POST"])
+def apiPredict():
+    if "year" not in request.get_json() or "month" not in request.get_json():
+        return {"Error": "Year and month is required"}, 400
+
+    data = request.get_json()
+    year = int(data['year'])
+    month = int(data['month'])
+    category = 0
+    type = 0
+
+    features = [[category, type, year, month]]
+    prediction = int(model.predict(features)[0])
+
+    return {'prediction': prediction}
+
 
 if __name__ == "__main__":
     app.run(debug=True)
